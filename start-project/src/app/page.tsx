fcd256335aa10b5d51c09997aa3d9d9a158842cf
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-02-06 13:07:16 -0300

    feat: conditional rendering

--- start-project/src/app/page.tsx
+++ start-project/src/app/page.tsx
@@ -7,14 +7,10 @@ function Page() {
       <h1 className="font-bold text-2xl">Hello, World!</h1>
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
 
-      <Card>
-        <>
-          <h3 className="text-3xl font-bold italic">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <p className="text-right text-sm">- JhoN Doe</p>
-        </>
-      </Card>
+      <Card
+        phrase="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        // author="Jhon Doe"
+      />
     </div>
   );
 }
