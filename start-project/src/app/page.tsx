254de58bf72b34a4257e3574314f6e8a58cf825f
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-02-06 11:42:16 -0300

    feat: passing props to a component

--- start-project/src/app/page.tsx
+++ start-project/src/app/page.tsx
@@ -6,7 +6,21 @@ function Page() {
       <h1 className="font-bold text-2xl">Hello, World!</h1>
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
 
-      <Person />
+      <Person
+        name="Nyan Cat"
+        avatar="https://media.discordapp.net/attachments/1184173326537588806/1204427270639452230/Collection11-20.gif?ex=65d4b14e&is=65c23c4e&hm=cd8bb8e65341330c50201d711d38f73ad0a011b1c2a292e5fd7a2fe49dd5a734&="
+        misc={["@NyanCat", "https://www.nyan.cat"]}
+      />
+      <Person
+        name="Pepe the Frog"
+        avatar="https://media.discordapp.net/attachments/1184173326537588806/1204436140892094474/wine-wine-time.gif?ex=65d4b991&is=65c24491&hm=4e48c814b2442b35bca733e5647d85c3951cd7e466d797bee7b1631bc2e05bad&="
+        misc={["@Pepe", "https://www.pepe.com"]}
+      />
+      <Person
+        name="Doge"
+        avatar="https://media.discordapp.net/attachments/1184173326537588806/1204436140522864701/shaking-doge.gif?ex=65d4b991&is=65c24491&hm=d0aa8978f4b987b1f0845b6291332ebb404d248095fb37aa92b5cb7f40949385&="
+        misc={["@Doge", "https://www.doge.com"]}
+      />
     </div>
   );
 }
--- start-project/src/components/Person.tsx
+++ start-project/src/components/Person.tsx
@@ -1,26 +1,17 @@
-const getWeekday = (today: Date) => {
-  return new Intl.DateTimeFormat("en", { weekday: "long" }).format(today);
+type Props = {
+  name: string;
+  avatar: string;
+  misc: string[];
 };
 
-export const Person = () => {
-  const data = {
-    name: "Nyan Cat",
-    avatar:
-      "https://media.discordapp.net/attachments/1184173326537588806/1204427270639452230/Collection11-20.gif?ex=65d4b14e&is=65c23c4e&hm=cd8bb8e65341330c50201d711d38f73ad0a011b1c2a292e5fd7a2fe49dd5a734&=",
-    misc: ["@NyanCat", "https://www.nyan.cat"],
-  };
-
-  const today: Date = new Date();
-
+export const Person = ({ name, avatar, misc }: Props) => {
   return (
     <>
-      <h1>
-        {data.name} - {getWeekday(today)}
-      </h1>
-      <img src={data.avatar} alt={data.name} className="w-60" />
+      <h1>{name}</h1>
+      <img src={avatar} alt={name} className="w-60" />
       <ul>
-        <li>Twitter: {data.misc[0]}</li>
-        <li>Website: {data.misc[1]}</li>
+        <li>Twitter: {misc[0]}</li>
+        <li>Website: {misc[1]}</li>
       </ul>
     </>
   );
