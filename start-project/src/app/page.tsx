4c7fa2de65f28847a31438b463f171fdf8d1948f
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-02-06 13:15:54 -0300

    feat: logical && operator

--- start-project/src/app/page.tsx
+++ start-project/src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Card } from "@/components/Card";
-import { Person } from "@/components/Person";
 
 function Page() {
   return (
--- start-project/src/components/Card.tsx
+++ start-project/src/components/Card.tsx
@@ -7,7 +7,12 @@ export const Card = ({ phrase, author }: Props) => {
   return (
     <div className="border-2 border-green-600 p-3 text-3xl text-center italic">
       <h3 className="text-3xl font-bold italic">"{phrase}"</h3>
-      <p className="text-right text-sm">- {author ?? "Unknown"}</p>
+      {author && (
+        <p className="text-right text-sm italic text-emerald-400">- {author}</p>
+      )}
+      {!author && (
+        <p className="text-right text-sm text-color text-pink-500">- Unknown</p>
+      )}
     </div>
   );
 };
