a547c880f91293082430ba89849949691c38078d
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-02-06 11:58:13 -0300

    feat: setting a default value in props

--- start-project/src/app/page.tsx
+++ start-project/src/app/page.tsx
@@ -11,11 +11,7 @@ function Page() {
         avatar="https://media.discordapp.net/attachments/1184173326537588806/1204427270639452230/Collection11-20.gif?ex=65d4b14e&is=65c23c4e&hm=cd8bb8e65341330c50201d711d38f73ad0a011b1c2a292e5fd7a2fe49dd5a734&="
         misc={["@NyanCat", "https://www.nyan.cat"]}
       />
-      <Person
-        name="Pepe the Frog"
-        avatar="https://media.discordapp.net/attachments/1184173326537588806/1204436140892094474/wine-wine-time.gif?ex=65d4b991&is=65c24491&hm=4e48c814b2442b35bca733e5647d85c3951cd7e466d797bee7b1631bc2e05bad&="
-        misc={["@Pepe", "https://www.pepe.com"]}
-      />
+      <Person name="Pepe the Frog" misc={["@Pepe", "https://www.pepe.com"]} />
       <Person
         name="Doge"
         avatar="https://media.discordapp.net/attachments/1184173326537588806/1204436140522864701/shaking-doge.gif?ex=65d4b991&is=65c24491&hm=d0aa8978f4b987b1f0845b6291332ebb404d248095fb37aa92b5cb7f40949385&="
--- start-project/src/components/Person.tsx
+++ start-project/src/components/Person.tsx
@@ -1,14 +1,18 @@
 type Props = {
   name: string;
-  avatar: string;
+  avatar?: string;
   misc: string[];
 };
 
-export const Person = ({ name, avatar, misc }: Props) => {
+export const Person = ({
+  name,
+  avatar = "https://media.discordapp.net/attachments/1184173326537588806/1204440561767940177/No_Image_345x2x.png?ex=65d4bdaf&is=65c248af&hm=ccd213b7cd8cbc9d85eab89cfaf691ce0b09e6190747f3feabb31fa5db1d89cf&=&format=webp&quality=lossless",
+  misc,
+}: Props) => {
   return (
     <div className="p-3">
       <h1>{name}</h1>
-      <img src={avatar} alt={name} className="w-60" />
+      <img src={avatar} alt={name} className="w-40" />
       <ul>
         <li>Twitter: {misc[0]}</li>
         <li>Website: {misc[1]}</li>
