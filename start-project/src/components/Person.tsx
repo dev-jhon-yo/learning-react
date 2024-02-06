fec3d6970fa1cdcd71fcdadf5ad1bdb17aaffd83
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-02-06 11:44:26 -0300

    feat: add padding between elements

--- start-project/src/components/Person.tsx
+++ start-project/src/components/Person.tsx
@@ -6,13 +6,13 @@ type Props = {
 
 export const Person = ({ name, avatar, misc }: Props) => {
   return (
-    <>
+    <div className="p-3">
       <h1>{name}</h1>
       <img src={avatar} alt={name} className="w-60" />
       <ul>
         <li>Twitter: {misc[0]}</li>
         <li>Website: {misc[1]}</li>
       </ul>
-    </>
+    </div>
   );
 };
