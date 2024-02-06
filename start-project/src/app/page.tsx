da84bd00b9fc73a7e8ddf2c5ca48acff1f12eb65
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-02-06 10:11:05 -0300

    feat: exporting and importing a component

--- start-project/src/app/page.tsx
+++ start-project/src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Square, Circle } from "@/components/Geometry";
+
 function Page() {
   return (
     <div>
       <h1 className="font-bold text-2xl">Hello, World!</h1>
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+
+      <Square />
+      <Circle />
     </div>
   );
 }
