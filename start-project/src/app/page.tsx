96d7eb4c55e7c590ebc68037b7855f3f11e543ba
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-02-07 11:12:01 -0300

    feat: filtering list

--- start-project/src/app/page.tsx
+++ start-project/src/app/page.tsx
@@ -1,19 +1,24 @@
 import { peopleList } from "@/data/peopleList";
 
 function Page() {
+  const developer = peopleList.filter(
+    (person) => person.profession === "developer"
+  );
+
   return (
     <div>
       <h1 className="font-bold text-2xl">Hello, World!</h1>
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
 
-      {peopleList.length > 0 && (
-        <ul>
-          {peopleList.map((person) => (
-            <li key={person.id}>
-              {person.name} - {person.profession}
-            </li>
-          ))}
-        </ul>
+      {developer.length > 0 && (
+        <>
+          <h3>DEVELOPER LIST FILTER</h3>
+          <ul>
+            {developer.map((person) => (
+              <li key={person.id}>{person.name}</li>
+            ))}
+          </ul>
+        </>
       )}
     </div>
   );
