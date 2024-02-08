9dda3ed157e1ae2c1a2526fdfc1979ceed4627ca
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-02-08 10:34:01 -0300

    feat: exercise conditional greeting 2

--- start-project/src/app/page.tsx
+++ start-project/src/app/page.tsx
@@ -5,20 +5,15 @@ function Page() {
   }).format();
 
   const hour = new Date().getHours();
-  let greeting = "";
-
-  if (hour >= 0 && hour < 12) {
-    greeting = "Good morning 😄";
-  } else if (hour >= 12 && hour < 18) {
-    greeting = "Good afternoon 😎";
-  } else if (hour >= 18 && hour <= 23) {
-    greeting = "Good evening 😴";
-  }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
       <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">{greeting}</div>
+      <div className="text-5xl font-bold">
+        {hour >= 0 && hour < 12 && "Good morning 😄"}
+        {hour >= 12 && hour < 18 && "Good afternoon 😎"}
+        {hour >= 18 && hour <= 23 && "Good evening 😴"}
+      </div>
     </div>
   );
 }
