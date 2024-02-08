38d55ce2cc370a3d80b8fa88397a3750fa564964
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-02-08 11:05:50 -0300

    feat: exercise emoji rating 1

--- start-project/src/app/page.tsx
+++ start-project/src/app/page.tsx
@@ -1,19 +1,9 @@
+import { EmojiRating } from "@/components/EmojiRating";
+
 function Page() {
-  const fullTime = new Intl.DateTimeFormat("en", {
-    timeStyle: "short",
-    hour12: false,
-  }).format();
-
-  const hour = new Date().getHours();
-
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
-      <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">
-        {hour >= 0 && hour < 12 && "Good morning 😄"}
-        {hour >= 12 && hour < 18 && "Good afternoon 😎"}
-        {hour >= 18 && hour <= 23 && "Good evening 😴"}
-      </div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <EmojiRating rate={3} />
     </div>
   );
 }
